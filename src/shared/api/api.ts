d813de33ed90542195868daa71b2e0d63d4c4a4c
rykush <actushima@gmail.com>
2026-04-17 11:29:48 +0300

    fix items for gh-pages

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/mock"
+  baseURL: `${import.meta.env.BASE_URL}mock`
 });
--- src/shared/config/navItems.ts
+++ src/shared/config/navItems.ts
@@ -1,20 +1,27 @@
+import chat from '@/icons/chat.svg'
+import chatActive from '@/icons/chat-active.svg'
+import users from '@/icons/users.svg'
+import usersActive from '@/icons/users-active.svg'
+import settings from '@/icons/settings.svg'
+import settingsActive from '@/icons/settings-active.svg'
+
 export const navItems = [
   {
     key: "chats",
     label: "Чаты",
-    icon: "../src/icons/chat.svg",
-    activeIcon: "../src/icons/chat-active.svg",
+    icon: chat,
+    activeIcon: chatActive,
   },
   {
     key: "contacts",
     label: "Контакты",
-    icon: "../src/icons/users.svg",
-    activeIcon: "../src/icons/users-active.svg",
+    icon: users,
+    activeIcon: usersActive,
   },
   {
     key: "settings",
     label: "Настройки",
-    icon: "../src/icons/settings.svg",
-    activeIcon: "../src/icons/settings-active.svg",
+    icon: settings,
+    activeIcon: settingsActive,
   },
 ] as const;
